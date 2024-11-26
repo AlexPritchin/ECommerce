e76494b8c29a7d1a382bebf3a98db7c43c9f6807
Oleksii Prytchyn <2.2alpr@gmail.com>
2024-11-26 18:53:15 +0200

    SideBar added

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,3 +1,77 @@
+import ArrowRight from '../assets/icons/arrowRight.svg';
+import './Home.scss';
+
 export const Home = () => {
-  return <div>Home page</div>;
+  return (
+    <div className='mainDiv'>
+      <div className='sidebarContainer'>
+        <div className='sidebar'>
+          <ul className='mainSidebarList'>
+            <li>
+              <a href='#'>
+                <div className='sideBarLinkContainer'>
+                  <span>Fashion</span>
+                  <ArrowRight />
+                </div>
+              </a>
+              <ul>
+                <li>
+                  <a href='#'>Shirts</a>
+                </li>
+                <li>
+                  <a href='#'>Bras</a>
+                </li>
+                <li>
+                  <a href='#'>Panties</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href='#'>
+                <div className='sideBarLinkContainer'>
+                  <span>Electronics</span>
+                  <ArrowRight />
+                </div>
+              </a>
+              <ul>
+                <li>
+                  <a href='#'>Smartphones</a>
+                </li>
+                <li>
+                  <a href='#'>Laptops</a>
+                </li>
+                <li>
+                  <a href='#'>TVs</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href='#'>Sports</a>
+            </li>
+            <li>
+              <a href='#'>Toys</a>
+            </li>
+            <li>
+              <a href='#'>Health</a>
+            </li>
+            <li>
+              <a href='#'>Home</a>
+            </li>
+            <li>
+              <a href='#'>Pets</a>
+            </li>
+            <li>
+              <a href='#'>Garden</a>
+            </li>
+            <li>
+              <a href='#'>Active</a>
+            </li>
+          </ul>
+        </div>
+        <div className='carouselContainer'>
+          <div className='carousel'></div>
+        </div>
+      </div>
+    </div>
+  );
 };
